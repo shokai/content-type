b18d69f53187f383d39f318e783a4df5bd7f1afe
Sho Hashimoto <hashimoto@shokai.org>
2012-06-26 01:09:49 +0900

    fix type_check test

--- tests/type_check.js
+++ tests/type_check.js
@@ -1,7 +1,9 @@
 
 var checker = require(__dirname+'/../helpers/type_check.js');
 
-var urls = ['http://localhost:5000', 'http://localhost:5000/images/shokai.png', 'http://localhost:5000/not_found'];
+var urls = ['http://shokai.org', 'http://twiticon.herokuapp.com/shokai'];
+console.log('check ' + urls.join(', '));
+
 
 for(var i = 0; i < urls.length; i++){
     (function(url){
@@ -12,8 +14,8 @@ for(var i = 0; i < urls.length; i++){
             console.error('error : '+err);
         });
         
-        checker.check(url, /^image\/.+/, function(res){
-            console.log(url + ' => is ' + (res ? 'image' : 'not image'));
+        checker.check(url, /^image\/.+/, function(ok){
+            console.log(url + ' => is ' + (ok ? 'image' : 'not image'));
         }, function(err, code){
             console.error(url + ' => response error!  status : ' + code);
             console.error('error : '+err);
