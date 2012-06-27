406509600088b19f59ff5eb6ea4b26ab879e3d3c
Sho Hashimoto <hashimoto@shokai.org>
2012-06-28 01:51:24 +0900

    cache errors 10min

--- helpers/type_check.js
+++ helpers/type_check.js
@@ -5,16 +5,21 @@ var cache = require('memory-cache');
 var self = this;
 
 this.type = function(url, callback, errback){
-    var type = cache.get(url);
-    if(type) callback(type);
+    var val = cache.get(url);
+    if(val){
+        if(val.type && typeof callback === 'function') callback(val.type);
+        else if(typeof errback === 'function') errback(val.error, val.statusCode);
+    }
     else{
         request.head(url, function (err, res, body) {
             if (!err && res.statusCode == 200) {
-                cache.put(url, res.headers['content-type'], 3600*4*1000);
+                cache.put(url, {type: res.headers['content-type']}, 3600*4*1000);
                 if(typeof callback === 'function') callback(res.headers['content-type']);
             }
             else{
-                if(typeof errback === 'function') errback(err, res !== undefined ? res.statusCode : null);
+                var statusCode = res !== undefined ? res.statusCode : null;
+                cache.put(url, {error: err, statusCode: statusCode}, 600*1000);
+                if(typeof errback === 'function') errback(err, statusCode);
             }
         });
     }
