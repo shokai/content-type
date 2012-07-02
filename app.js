24cd7696f3c01bba07cc90a04f0a3cfa4860cd1e
Sho Hashimoto <hashimoto@shokai.org>
2012-07-03 00:43:13 +0900

    move routing to routes/

--- app.js
+++ app.js
@@ -5,12 +5,10 @@
 
 var express = require('express');
 
-var routes = {
-    index: require('./routes'),
-    check_type: require('./routes/check_type')
-};
+var app = process.app = module.exports = express.createServer();
 
-var app = module.exports = express.createServer();
+require('./routes');
+require('./routes/check_type');
 
 // Configuration
 
@@ -31,11 +29,6 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
-
-app.get('/', routes.index.index);
-app.get(/^\/type\/(https?:\/\/.+)/, routes.check_type.get_type);
-app.get(/^\/type\/(.+)\/(https?:\/\/.+)/, routes.check_type.check_type);
 
 app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/check_type.js
+++ routes/check_type.js
@@ -1,27 +1,28 @@
 var checker = require(process.env.PWD+'/helpers/type_check');
 
-exports.get_type = function(req, res){
-    var url = req.params[0];
-    checker.type(url, function(content_type){
-        res.send(content_type, 200);
-    }, function(err, code){
-        res.send('error('+code+')', 500);
-    });
-};
+process.app.get(/^\/type\/(https?:\/\/.+)/, function(req, res){
+  var url = req.params[0];
+  checker.type(url, function(content_type){
+    res.send(content_type, 200);
+  }, function(err, code){
+    res.send('error('+code+')', 500);
+  });
+});
 
-exports.check_type = function(req, res){
-    var app_root = (req.connection.encrypted ? 'https' : 'http') + '://' + req.headers['host'];
 
-    var type = req.params[0];
-    var url = req.params[1];
-    checker.check(url, new RegExp('^'+type), function(ok){
-        if(ok) res.redirect(url);
-        else{
-            if(type.match(/^image/i)) res.redirect(app_root+'/images/invalid.png');
-            else res.send('content type error', 415);
-        }
-    }, function(err, code){
-        if(type.match(/^image/i)) res.redirect(app_root+'/images/invalid.png');
-        else res.send('error('+code+')', 500);
-    });
-};
+process.app.get(/^\/type\/(.+)\/(https?:\/\/.+)/, function(req, res){
+  var app_root = (req.connection.encrypted ? 'https' : 'http') + '://' + req.headers['host'];
+
+  var type = req.params[0];
+  var url = req.params[1];
+  checker.check(url, new RegExp('^'+type), function(ok){
+    if(ok) res.redirect(url);
+    else{
+      if(type.match(/^image/i)) res.redirect(app_root+'/images/invalid.png');
+      else res.send('content type error', 415);
+    }
+  }, function(err, code){
+    if(type.match(/^image/i)) res.redirect(app_root+'/images/invalid.png');
+    else res.send('error('+code+')', 500);
+  });
+});
