7bace59c721d292fb14abee1d20f9ee3d78782ad
Sho Hashimoto <hashimoto@shokai.org>
2012-07-15 10:33:26 +0900

    use memjs

--- helpers/type_check.js
+++ helpers/type_check.js
@@ -1,28 +1,39 @@
 // HTTP Status Checker
 
 var request = require('request');
-var cache = require('memory-cache');
+var memjs = require('memjs');
+var cache = new memjs.Client.create(null, {expires: 3600*4});
 var self = this;
 
 this.type = function(url, callback, errback){
-  var val = cache.get(url);
-  if(val){
-    if(val.type && typeof callback === 'function') callback(val.type);
-    else if(typeof errback === 'function') errback(val.error, val.statusCode);
-  }
-  else{
-    request.head(url, function (err, res, body) {
-      if (!err && res.statusCode == 200) {
-        cache.put(url, {type: res.headers['content-type']}, 3600*4*1000);
-        if(typeof callback === 'function') callback(res.headers['content-type']);
-      }
-      else{
-        var statusCode = res !== undefined ? res.statusCode : null;
-        cache.put(url, {error: err, statusCode: statusCode}, 600*1000);
-        if(typeof errback === 'function') errback(err, statusCode);
-      }
-    });
-  }
+  cache.get(url, function(err, val, flag){
+    if(!err && val){
+      val = JSON.parse(val.toString());
+      if(typeof callback === 'function') callback(val.type);
+      else if(typeof errback === 'function') errback(val.error, val.statusCode);
+    }
+    else{
+      request.head(url, function(err, res, body){
+        if(!err && res.statusCode == 200){
+          cache.set(url,
+                    JSON.stringify({type: res.headers['content-type']}),
+                    function(err, val){
+            if(err) console.error('cache set error');
+          });
+          if(typeof callback === 'function') callback(res.headers['content-type']);
+        }
+        else{
+          var statusCode = (res !== undefined ? res.statusCode : null);
+          cache.set(url,
+                    JSON.stringify({error: err, statusCode: statusCode}),
+                    function(err, val){
+            if(err) console.error('cache set error');
+          });
+          if(typeof errback === 'function') errback(err, statusCode);
+        }
+      });
+    }
+  });
 };
 
 this.check = function(url, content_type, callback, errback){
