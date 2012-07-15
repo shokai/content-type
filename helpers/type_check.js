c2ca2caacca1817aef93bba2f16798fba67f7cb5
Sho Hashimoto <hashimoto@shokai.org>
2012-07-15 09:27:40 +0900

    fix indent

--- helpers/type_check.js
+++ helpers/type_check.js
@@ -5,30 +5,30 @@ var cache = require('memory-cache');
 var self = this;
 
 this.type = function(url, callback, errback){
-    var val = cache.get(url);
-    if(val){
-        if(val.type && typeof callback === 'function') callback(val.type);
-        else if(typeof errback === 'function') errback(val.error, val.statusCode);
-    }
-    else{
-        request.head(url, function (err, res, body) {
-            if (!err && res.statusCode == 200) {
-                cache.put(url, {type: res.headers['content-type']}, 3600*4*1000);
-                if(typeof callback === 'function') callback(res.headers['content-type']);
-            }
-            else{
-                var statusCode = res !== undefined ? res.statusCode : null;
-                cache.put(url, {error: err, statusCode: statusCode}, 600*1000);
-                if(typeof errback === 'function') errback(err, statusCode);
-            }
-        });
-    }
+  var val = cache.get(url);
+  if(val){
+    if(val.type && typeof callback === 'function') callback(val.type);
+    else if(typeof errback === 'function') errback(val.error, val.statusCode);
+  }
+  else{
+    request.head(url, function (err, res, body) {
+      if (!err && res.statusCode == 200) {
+        cache.put(url, {type: res.headers['content-type']}, 3600*4*1000);
+        if(typeof callback === 'function') callback(res.headers['content-type']);
+      }
+      else{
+        var statusCode = res !== undefined ? res.statusCode : null;
+        cache.put(url, {error: err, statusCode: statusCode}, 600*1000);
+        if(typeof errback === 'function') errback(err, statusCode);
+      }
+    });
+  }
 };
 
 this.check = function(url, content_type, callback, errback){
-    self.type(url, function(res){
-        if(typeof callback === 'function') callback(res.match(content_type) !== null);
-    }, function(err, code){
-        if(typeof errback === 'function') errback(err, code);
-    });
+  self.type(url, function(res){
+    if(typeof callback === 'function') callback(res.match(content_type) !== null);
+  }, function(err, code){
+    if(typeof errback === 'function') errback(err, code);
+  });
 };
--- tests/type_check.js
+++ tests/type_check.js
@@ -6,19 +6,19 @@ console.log('check ' + urls.join(', '));
 
 
 for(var i = 0; i < urls.length; i++){
-    (function(url){
-        checker.type(url, function(res){
-            console.log(url + ' => content-type : '+res);
-        }, function(err, code){
-            console.error(url + ' => response error!  status : ' + code);
-            console.error('error : '+err);
-        });
-        
-        checker.check(url, /^image\/.+/, function(ok){
-            console.log(url + ' => is ' + (ok ? 'image' : 'not image'));
-        }, function(err, code){
-            console.error(url + ' => response error!  status : ' + code);
-            console.error('error : '+err);
-        });
-    })(urls[i]);
+  (function(url){
+    checker.type(url, function(res){
+      console.log(url + ' => content-type : '+res);
+    }, function(err, code){
+      console.error(url + ' => response error!  status : ' + code);
+      console.error('error : '+err);
+    });
+
+    checker.check(url, /^image\/.+/, function(ok){
+      console.log(url + ' => is ' + (ok ? 'image' : 'not image'));
+    }, function(err, code){
+      console.error(url + ' => response error!  status : ' + code);
+      console.error('error : '+err);
+    });
+  })(urls[i]);
 };
